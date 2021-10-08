e38d8035ca5cb9e2c6748fc1d3ee1fd19c841cca
dahlo <m.dahlo@gmail.com>
2021-10-08 10:19:09 +0200

    News and events

--- layouts/index.vue
+++ layouts/index.vue
@@ -3,12 +3,16 @@
     <NavBar />
     <nuxt />
 
-    <section class="flex flex-row">
-      <h3 class="text-primary-600 dark:text-primary-400 mx-auto">Latest news</h3>
-      <posts post-type="blog" :amount="5" />
-      <h3 class="text-primary-600 dark:text-primary-400 mx-auto">Events</h3>
-      <posts post-type="blog" :amount="5" />
-    </section>
+    <div class="flex flex-row">
+      <section class="mt-4">
+        <h3 class="text-primary-600 dark:text-primary-400 mx-auto">Latest news</h3>
+        <posts post-type="blog" :amount="5" />
+      </section>
+      <section class="mt-4">
+        <h3 class="text-primary-600 dark:text-primary-400 mx-auto">Events</h3>
+        <posts post-type="blog" :amount="5" />
+      </section>
+    </div>
 
     <ColorModePicker />
   </div>
