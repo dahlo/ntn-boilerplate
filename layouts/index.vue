76fa1c99e342b86c1289f14b1783ba92fb62f317
dahlo <m.dahlo@gmail.com>
2021-10-08 11:36:12 +0200

    News and events

--- layouts/index.vue
+++ layouts/index.vue
@@ -3,16 +3,17 @@
     <NavBar />
     <nuxt />
 
-    <div class="grid grid-flow-col gap-3">
-      <section class="mt-4">
-        <h3 class="text-primary-600 dark:text-primary-400 mx-auto">Latest news</h3>
-        <posts post-type="blog" :amount="5" />
-      </section>
-      <section class="mt-4">
-        <h3 class="text-primary-600 dark:text-primary-400 mx-auto">Events</h3>
-        <posts post-type="blog" :amount="5" />
-      </section>
-    </div>
+    <div class="container mx-auto">
+      <div class="grid grid-cols-1 md:grid-cols-2">
+        <div class="flex justify-center text-6xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100">
+          <h3 class="text-primary-600 dark:text-primary-400 mx-auto">Latest news</h3>
+          <posts post-type="blog" :amount="5" />
+        </div>
+        <div class="flex justify-center text-6xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100">
+          <h3 class="text-primary-600 dark:text-primary-400 mx-auto">Events</h3>
+          <posts post-type="blog" :amount="5" />
+        </div>
+      </div>
 
     <ColorModePicker />
   </div>
