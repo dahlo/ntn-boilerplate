675346eed73fb10d4aba035dd17a78363c61a4ad
dahlo <m.dahlo@gmail.com>
2021-10-08 11:38:45 +0200

    News and events

--- layouts/index.vue
+++ layouts/index.vue
@@ -6,12 +6,16 @@
     <div class="container mx-auto">
       <div class="grid grid-cols-1 md:grid-cols-2">
         <div class="flex justify-center text-6xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100">
-          <h3 class="text-primary-600 dark:text-primary-400 mx-auto">Latest news</h3>
-          <posts post-type="blog" :amount="5" />
+          <section class="mt-4">
+            <h3 class="text-primary-600 dark:text-primary-400 mx-auto">Latest news</h3>
+            <posts post-type="blog" :amount="5" />
+          </section>
         </div>
         <div class="flex justify-center text-6xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100">
-          <h3 class="text-primary-600 dark:text-primary-400 mx-auto">Events</h3>
-          <posts post-type="blog" :amount="5" />
+          <section class="mt-4">
+            <h3 class="text-primary-600 dark:text-primary-400 mx-auto">Events</h3>
+            <posts post-type="blog" :amount="5" />
+          </section>
         </div>
       </div>
 
