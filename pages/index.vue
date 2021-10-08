3011ba81878b868dfe06ac69a269144b4dd9000e
dahlo <m.dahlo@gmail.com>
2021-10-08 09:44:43 +0200

    News and events

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,17 @@
 <template>
   <main>
     <section class="self-center flex flex-col flex-1 items-center justify-center">
-      <h1 class="title text-center">Nuxt — Tailwind — Netlify CMS</h1>
-      <h2 class="subtitle text-center">Boilerplate</h2>
+      <h1 class="title text-center">NBIS</h1>
+      <h2 class="subtitle text-center">National Bioinformatic Infrastructure Sweden</h2>
     </section>
 
-    <section class="mt-8">
-      <h3 class="text-primary-600 dark:text-primary-400 max-w-5xl mx-auto">Latest blog post</h3>
-      <posts post-type="blog" :amount="1" />
+    <section class="mt-4">
+      <h3 class="text-primary-600 dark:text-primary-400 max-w-5xl mx-auto">Latest news</h3>
+      <posts post-type="blog" :amount="5" />
+    </section>
+    <section class="mt-4">
+      <h3 class="text-primary-600 dark:text-primary-400 max-w-5xl mx-auto">Events</h3>
+      <posts post-type="blog" :amount="5" />
     </section>
   </main>
 </template>
