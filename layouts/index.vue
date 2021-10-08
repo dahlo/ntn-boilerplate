6ddda3bd6ece8aac6f39fdabe89abf7bfd9f97b0
dahlo <m.dahlo@gmail.com>
2021-10-08 09:57:48 +0200

    News and events

---
+++ layouts/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper">
+    <NavBar />
+    <nuxt />
+
+    <section class="mt-4">
+      <h3 class="text-primary-600 dark:text-primary-400 max-w-1/2 mx-auto">Latest news</h3>
+      <posts post-type="blog" :amount="5" />
+    </section>
+    <section class="mt-4">
+      <h3 class="text-primary-600 dark:text-primary-400 max-w-1/2 mx-auto">Events</h3>
+      <posts post-type="blog" :amount="5" />
+    </section>
+
+    <ColorModePicker />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,5 @@
       <h1 class="title text-center">NBIS</h1>
       <h2 class="subtitle text-center">National Bioinformatic Infrastructure Sweden</h2>
     </section>
-
-    <section class="mt-4">
-      <h3 class="text-primary-600 dark:text-primary-400 max-w-5xl mx-auto">Latest news</h3>
-      <posts post-type="blog" :amount="5" />
-    </section>
-    <section class="mt-4">
-      <h3 class="text-primary-600 dark:text-primary-400 max-w-5xl mx-auto">Events</h3>
-      <posts post-type="blog" :amount="5" />
-    </section>
   </main>
 </template>
