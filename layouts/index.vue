f88dcf67ebc058c6133a480a2f2b0bb6c48a3e99
dahlo <m.dahlo@gmail.com>
2021-10-08 10:34:58 +0200

    News and events

--- layouts/index.vue
+++ layouts/index.vue
@@ -3,7 +3,7 @@
     <NavBar />
     <nuxt />
 
-    <div class="flex flex-row justify-center">
+    <div class="grid grid-flow-col gap-3">
       <section class="mt-4">
         <h3 class="text-primary-600 dark:text-primary-400 mx-auto">Latest news</h3>
         <posts post-type="blog" :amount="5" />
